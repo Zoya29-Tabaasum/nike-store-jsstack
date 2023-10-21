96080f9cfe8ea34f74613d0e6632fac00ac26f54
Zoya29-Tabaasum <114081402+Zoya29-Tabaasum@users.noreply.github.com>
2023-10-22 00:15:00 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,9 +24,7 @@ const Footer = ({ footerAPI: { titles, links } }) => {
               </ul>
             ))}
           </div>
-          <div className='mt-5 text-center'>
-            <p className='text-sm md:text-center'>Copyright<sup className='text-base font-bold'>&copy;</sup> All Reserved Rights <span className='font-semibold'>JSSTACK DEVELOPERS {Year}</span></p>
-          </div>
+          
         </div>
       </footer>
    </>
